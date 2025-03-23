2ef3d64b689d53820eac325e2c96926cd5f31201
gimhansandeeptha <rpgimhansandeeptha@gmail.com>
2025-03-23 14:43:57 +0530

    minor changes

--- tools/eval/datasets/imagenet.py
+++ tools/eval/datasets/imagenet.py
@@ -13,7 +13,7 @@ class ImageNet:
         self.top5_acc = -1
 
         self.root_val = os.path.join(self.root, "imagenet-val", "val")
-        self.val_label_file = os.path.join(self.root, "caffe_ilsvrc12", "val.txt")
+        self.val_label_file = os.path.join(self.root, "caffe-imagenet", "val.txt")
 
         self.val_label = self.load_label(self.val_label_file)
 
